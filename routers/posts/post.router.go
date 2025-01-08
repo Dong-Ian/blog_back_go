06d117af6bc4154caf92c89f517d6d652b88cfe6
donghquinn <ehdgus1524@gmail.com>
2025-01-08 23:45:41 +0900

    post

--- routers/posts/post.router.go
+++ routers/posts/post.router.go
@@ -8,14 +8,14 @@ import (
 )
 
 func PostRouter(server *mux.Router) {
-	server.HandleFunc("POST /post/contents", controllers.PostContentsController).Methods(http.MethodPost)
-	server.HandleFunc("POST /post/list", controllers.GetPostController).Methods(http.MethodPost)
-	server.HandleFunc("POST /post/list/pinned", controllers.GetPinnedPostController).Methods(http.MethodPost)
+	server.HandleFunc("/post/contents", controllers.PostContentsController).Methods(http.MethodPost)
+	server.HandleFunc("/post/list", controllers.GetPostController).Methods(http.MethodPost)
+	server.HandleFunc("/post/list/pinned", controllers.GetPinnedPostController).Methods(http.MethodPost)
 
-	server.HandleFunc("POST /post/list/tag", controllers.GetPostsByTagController).Methods(http.MethodPost)
-	server.HandleFunc("POST /post/list/category", controllers.GetPostsByCategoryController).Methods(http.MethodPost)
+	server.HandleFunc("/post/list/tag", controllers.GetPostsByTagController).Methods(http.MethodPost)
+	server.HandleFunc("/post/list/category", controllers.GetPostsByCategoryController).Methods(http.MethodPost)
 
-	server.HandleFunc("POST /post/url", controllers.GetImageUrl).Methods(http.MethodPost)
+	server.HandleFunc("/post/url", controllers.GetImageUrl).Methods(http.MethodPost)
 
-	server.HandleFunc("POST /post/category/list", controllers.GetCategoryController).Methods(http.MethodPost)
+	server.HandleFunc("/post/category/list", controllers.GetCategoryController).Methods(http.MethodPost)
 }
