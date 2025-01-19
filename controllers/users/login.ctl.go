7c0e763ef2d36e0be6a4b07db38d6be52d018e6f
donghquinn <ehdgus1524@gmail.com>
2025-01-19 23:33:58 +0900

    secure cookie

--- controllers/users/login.ctl.go
+++ controllers/users/login.ctl.go
@@ -107,16 +107,16 @@ func LoginController(res http.ResponseWriter, req *http.Request) {
 	accessTokenCookie := http.Cookie{
 		Name:   "accessToken",
 		Value:  accessToken,
-		Secure: true,
-		// HttpOnly: true,
+		Secure: false, // 로컬 환경에서는 FALSE, 실제에서는 TRUE
+		// HttpOnly: true, // 로컬 환경에서는 FALSE, 실제에서는 TRUE
 		SameSite: http.SameSiteNoneMode,
 	}
 
 	refreshTokenCookie := http.Cookie{
 		Name:   "refreshToken",
 		Value:  refreshToken,
-		Secure: true,
-		// HttpOnly: true,
+		Secure: false, // 로컬 환경에서는 FALSE, 실제에서는 TRUE
+		// HttpOnly: true, // 로컬 환경에서는 FALSE, 실제에서는 TRUE
 		SameSite: http.SameSiteNoneMode,
 	}
 
