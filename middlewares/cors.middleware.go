3135c26fa769f3e900a3274ec20cdf12e054cc7e
donghquinn <ehdgus1524@gmail.com>
2025-01-19 21:29:33 +0900

    cors

--- middlewares/cors.middleware.go
+++ middlewares/cors.middleware.go
@@ -35,12 +35,14 @@ func CorsMiddlewares(next http.Handler) http.Handler {
 
 		if isAllowed {
 			res.Header().Set("Access-Control-Allow-Origin", origin)
+			res.Header().Set("Access-Control-Allow-Credentials", "true")
+		} else {
+			log.Printf("Origin not allowed: %s", origin)
 		}
 
 		res.Header().Set("Access-Control-Max-Age", "86400")
 		res.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 		res.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		res.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		// Handle preflight request
 		if req.Method == http.MethodOptions {
