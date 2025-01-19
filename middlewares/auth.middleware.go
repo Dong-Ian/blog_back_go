1f1994b8721599147d320282fec0d04173aeebd1
donghquinn <ehdgus1524@gmail.com>
2025-01-19 23:22:01 +0900

    auth  middelware

--- middlewares/auth.middleware.go
+++ middlewares/auth.middleware.go
@@ -38,6 +38,8 @@ var excludeRouteList = []string{
 // AuthMiddleware는 accessToken 쿠키를 추출하고 JWT를 검증하는 미들웨어입니다.
 func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Auth Middleware: %s", r.URL.Path)
+
 		// 제외할 경로는 바로 다음 핸들러로 넘김
 		for _, route := range excludeRouteList {
 			if strings.HasPrefix(r.URL.Path, route) {
