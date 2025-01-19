ccd2a5ae049d74530e99bd84ca76d52c3557ee9a
donghquinn <ehdgus1524@gmail.com>
2025-01-19 22:29:00 +0900

    cors

--- middlewares/cors.middleware.go
+++ middlewares/cors.middleware.go
@@ -1,20 +1,39 @@
 package middlewares
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-// var originList = []string{
-// 	"http://localhost:3000",
-// 	"https://blog.minjae-dev.com/",
-// 	"https://blog.donghyuns.com",
-// 	"unknown",
-// }
+var originList = []string{
+	"http://localhost:3000",
+	"https://blog.minjae-dev.com/",
+	"https://blog.donghyuns.com",
+	"unknown",
+}
 
 func CorsMiddlewares(next *mux.Router) http.Handler {
 	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		origin := req.Header.Get("Origin")
+
+		if origin == "" {
+			origin = "unknown"
+		}
+
+		for _, o := range originList {
+			if o == origin {
+				log.Printf("Allowed Origin: %s", origin)
+				res.Header().Set("Access-Control-Allow-Origin", origin)
+				res.Header().Set("Access-Control-Max-Age", "86400")
+				res.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+				res.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+				res.Header().Set("Access-Control-Allow-Credentials", "true")
+				break
+			}
+		}
+
 		res.Header().Set("Access-Control-Allow-Origin", "*")
 		res.Header().Set("Access-Control-Max-Age", "86400")
 		res.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
