89402f4103cf5959cff8bf4aa6bac20e263bc6d6
donghquinn <ehdgus1524@gmail.com>
2025-01-19 21:02:16 +0900

    cors set

--- middlewares/cors.middleware.go
+++ middlewares/cors.middleware.go
@@ -4,9 +4,28 @@ import (
 	"net/http"
 )
 
+var originList = []string{
+	"http://localhost:3000",
+	"https://blog.minjae-dev.com/",
+	"https://blog.donghyuns.com",
+}
+
 func CorsMiddlewares(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
-		res.Header().Set("Access-Control-Allow-Origin", "*")
+		origin := req.Header.Get("Origin")
+		// 요청의 Origin이 허용된 Origin 목록에 있는지 확인
+		isAllowed := false
+		for _, allowedOrigin := range originList {
+			if origin == allowedOrigin {
+				isAllowed = true
+				break
+			}
+		}
+
+		if isAllowed {
+			res.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+
 		res.Header().Set("Access-Control-Max-Age", "86400")
 		res.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 		res.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
